b5af6b19dd218dfe26bb4199eb4f5cfa752ac89d
admin <-->
2022-12-08 04:58:34 +0000

    postgres test

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,4 @@
-import click
+
 import psycopg2
 
 from feast.infra.utils.postgres.connection_utils import df_to_postgres_table
@@ -13,8 +13,9 @@ def bootstrap():
 
     from feast.driver_test_data import create_driver_hourly_stats_df
 
-    repo_path = pathlib.Path(__file__).parent.absolute()
-    config_file = repo_path / "feature_store.yaml"
+    #repo_path = pathlib.Path(__file__).parent.absolute()
+    #repo_path ="."
+    config_file = "feature_store.yaml"
 
     end_date = datetime.now().replace(microsecond=0, second=0, minute=0)
     start_date = end_date - timedelta(days=15)
@@ -22,41 +23,47 @@ def bootstrap():
     driver_entities = [1001, 1002, 1003, 1004, 1005]
     driver_df = create_driver_hourly_stats_df(driver_entities, start_date, end_date)
 
-    postgres_host = click.prompt("Postgres host", default="localhost")
-    postgres_port = click.prompt("Postgres port", default="5432")
-    postgres_database = click.prompt("Postgres DB name", default="postgres")
-    postgres_schema = click.prompt("Postgres schema", default="public")
-    postgres_user = click.prompt("Postgres user")
-    postgres_password = click.prompt("Postgres password", hide_input=True)
+    # postgres_host = click.prompt("Postgres host", default="localhost")
+    # postgres_port = click.prompt("Postgres port", default="5432")
+    # postgres_database = click.prompt("Postgres DB name", default="postgres")
+    # postgres_schema = click.prompt("Postgres schema", default="public")
+    # postgres_user = click.prompt("Postgres user")
+    # postgres_password = click.prompt("Postgres password", hide_input=True)
+    postgres_host = "172.21.173.212"
+    postgres_port = "5432"
+    postgres_database = "sampledb"
+    postgres_schema = "public"
+    postgres_user = "userYNG"
+    postgres_password = "2VhoUSlfty5hH7vY"
 
-    if click.confirm(
-        'Should I upload example data to Postgres (overwriting "feast_driver_hourly_stats" table)?',
-        default=True,
-    ):
-        db_connection = psycopg2.connect(
-            dbname=postgres_database,
-            host=postgres_host,
-            port=int(postgres_port),
-            user=postgres_user,
-            password=postgres_password,
-            options=f"-c search_path={postgres_schema}",
-        )
+    # if click.confirm(
+    #     'Should I upload example data to Postgres (overwriting "feast_driver_hourly_stats" table)?',
+    #     default=True,
+    # ):
+    db_connection = psycopg2.connect(
+        dbname=postgres_database,
+        host=postgres_host,
+        port=int(postgres_port),
+        user=postgres_user,
+        password=postgres_password,
+        options=f"-c search_path={postgres_schema}",
+    )
 
-        with db_connection as conn, conn.cursor() as cur:
+    with db_connection as conn, conn.cursor() as cur:
             cur.execute('DROP TABLE IF EXISTS "feast_driver_hourly_stats"')
 
-        df_to_postgres_table(
+    df_to_postgres_table(
             config=PostgreSQLConfig(
-                host=postgres_host,
-                port=int(postgres_port),
-                database=postgres_database,
-                db_schema=postgres_schema,
-                user=postgres_user,
-                password=postgres_password,
-            ),
-            df=driver_df,
-            table_name="feast_driver_hourly_stats",
-        )
+            host=postgres_host,
+            port=int(postgres_port),
+            database=postgres_database,
+            db_schema=postgres_schema,
+            user=postgres_user,
+            password=postgres_password,
+        ),
+        df=driver_df,
+        table_name="feast_driver_hourly_stats",
+    )
 
     replace_str_in_file(config_file, "DB_HOST", postgres_host)
     replace_str_in_file(config_file, "DB_PORT", postgres_port)
